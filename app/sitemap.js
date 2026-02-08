e77a44b2cac64b72132630158ab173062d25df90
priyamaddheshiya <priya13997@gmail.com>
2026-02-08 22:04:36 +0530

    added the scripts for seo

---
+++ app/sitemap.js
@@ -0,0 +1,16 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://healthvandanam.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://healthvandanam.com/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://healthvandanam.com/surgery',
+      lastModified: new Date(),
+    },
+  ];
+}
